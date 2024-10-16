11ba2ee66c104af754a1b0f1b455983df6599d01
23DCS040_HARMIT <harmitjetani@gmail.com>
2024-10-16 09:26:10 +0530

    Create prac_13.java

---
+++ set 4/prac_13.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class Employee
+{
+Scanner s=new Scanner(System.in);
+ 
+String fs;
+String ls;
+Double salary;
+ 
+Employee()
+{
+}
+ Employee( String f,String l ,Double s)
+{
+ 
+fs=f;
+ls=l;
+salary=s;
+ 
+}
+void setsfs()
+{
+fs=s.nextLine();
+ 
+}
+void setsls()
+{
+ls=s.nextLine();
+ 
+}
+void setssalary()
+{
+salary=s.nextDouble();
+ 
+ 
+   salary=salary*(10.0/100.0);
+ 
+ 
+ 
+ 
+}
+String getsfs()
+{
+return fs;
+ 
+}
+String getsls()
+{
+return ls;
+ 
+}
+Double getssalary()
+{
+return salary;
+ 
+ 
+}
+ 
+ 
+ 
+ 
+}
+ 
+ 
+class prac_13
+{
+public static void main(String[]args)
+{
+ 
+Employee E1=new Employee();
+E1.setsfs();
+E1.setsls();
+E1.setssalary();
+System.out.println(E1.getsfs());
+System.out.println(E1.getsls());
+System.out.println(E1.getssalary());
+ 
+ 
+ 
+}
+ 
+}
