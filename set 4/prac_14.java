483e0f604634fd371aa48235208ca0ab9dd514d0
23DCS040_HARMIT <harmitjetani@gmail.com>
2024-10-16 09:27:00 +0530

    Create prac_14.java

---
+++ set 4/prac_14.java
@@ -0,0 +1,66 @@
+import java.util.*;
+class Date
+{
+int date,month,year;
+Scanner s=new Scanner(System.in);
+Date()
+{
+ 
+ 
+}
+Date(int d,int m, int y)
+ 
+{
+ date=d;
+month=m;
+year=y;
+ 
+}
+ 
+void setsdate()
+{
+ date=s.nextInt();
+}
+void setsmonth()
+{
+ month=s.nextInt();
+}
+void setsyear()
+{
+ year=s.nextInt();
+}
+int getsdate()
+{
+return date;
+}
+int getsmonth()
+{
+return month;
+}
+int getsyear()
+{
+return year;
+}
+void display(){
+ 
+System.out.print(date+"/"+month+"/"+year);
+ 
+}
+ 
+}
+ 
+class prac_14{
+ 
+public static void main(String[]args)
+{
+Date D=new Date();
+D.setsdate(); 
+D.setsmonth();
+D.setsyear();
+D.display();
+ 
+ 
+ 
+}
+ 
+}
